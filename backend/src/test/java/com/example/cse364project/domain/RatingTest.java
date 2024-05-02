d02404999a485a6622d6a2a0102e9819d8806693
sumin <sumin6196@gmail.com>
2024-05-03 01:26:17 +0900

    chore: RatingTest 100퍼센트 만들어둠 영찬 코드에서 조금 추가함

--- backend/src/test/java/com/example/cse364project/domain/RatingTest.java
+++ backend/src/test/java/com/example/cse364project/domain/RatingTest.java
@@ -1,5 +1,8 @@
 package com.example.cse364project.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +17,7 @@ public class RatingTest {
 
         Rating rating = new Rating(movieId, userId, rate, timestamp);
 
+        Assertions.assertEquals(rating.getId(), rating.getId());
         Assertions.assertEquals(movieId, rating.getMovieId());
         Assertions.assertEquals(userId, rating.getUserId());
         Assertions.assertEquals(rate, rating.getRate());
@@ -24,16 +28,19 @@ public class RatingTest {
     public void testRatingSetters() {
         Rating rating = new Rating("123", "456", 5, "2022-01-01");
 
+        String newId = "id";
         String newMovieId = "789";
         String newUserId = "012";
         int newRate = 4;
         String newTimestamp = "2022-02-01";
 
+        rating.setId(newId);
         rating.setMovieId(newMovieId);
         rating.setUserId(newUserId);
         rating.setRate(newRate);
         rating.setTimestamp(newTimestamp);
 
+        Assertions.assertEquals(newId, rating.getId());
         Assertions.assertEquals(newMovieId, rating.getMovieId());
         Assertions.assertEquals(newUserId, rating.getUserId());
         Assertions.assertEquals(newRate, rating.getRate());
@@ -54,5 +61,20 @@ public class RatingTest {
 
         Assertions.assertEquals(rating1.hashCode(), rating2.hashCode());
         Assertions.assertNotEquals(rating1.hashCode(), rating3.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        Rating rating = new Rating("123", "456", 5, "2022-01-01");
+        Assertions.assertEquals(rating.toString(), "Rating{" + "id='" + rating.getId() + '\'' + ", movieId=" + "123" + ", userId=" + "456" + ", rate=" + 5 + ", timestamp=" + "2022-01-01" + '}');
+    }
+
+    @Test
+    public void testEquals() {
+        Rating rating1 = new Rating("123", "456", 5, "2022-01-01");
+        Rating rating2 = new Rating("123", "456", 5, "2022-01-01");
+        Assertions.assertTrue(rating1.equals(rating1));
+        Assertions.assertTrue(rating1.equals(rating2));
+        Assertions.assertFalse(rating1.equals(null));
     }
 }
