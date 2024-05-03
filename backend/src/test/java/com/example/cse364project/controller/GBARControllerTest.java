6bfa42d9334d8b5ba22cd85845f218a0eb6a906a
queso-gato1355 <solar767@unist.ac.kr>
2024-05-03 17:30:38 +0900

    test : Controller test 추가

--- backend/src/test/java/com/example/cse364project/controller/GBARControllerTest.java
+++ backend/src/test/java/com/example/cse364project/controller/GBARControllerTest.java
@@ -42,7 +42,7 @@ public class GBARControllerTest {
     }
 
     @Test
-    public void testGetOneActor() throws Exception {
+    public void testGetOneActor1() throws Exception {
         List<String> movies = Arrays.asList("Airplane!", "Airplane II: The Sequel");
         Actor actor = new Actor("Robert Hays", movies, new int[]{2, 0, 0, 2}, new double[]{0.1, 0.0, 0.0, 0.3});
         when(gbarService.getActorByName(anyString())).thenReturn(actor);
@@ -55,7 +55,20 @@ public class GBARControllerTest {
     }
 
     @Test
-    public void testRecommendActor() throws Exception {
+    public void testGetOneActor2() throws Exception {
+        List<String> movies = Arrays.asList("Animal House", "The Blues Brothers");
+        Actor actor = new Actor("John Belushi", movies, new int[]{2, 0, 0, 2}, new double[]{0.1, 0.0, 0.0, 0.3});
+        when(gbarService.getActorByName(anyString())).thenReturn(actor);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/gbar/find")
+                .param("name", "Robert Hays")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("John Belushi"));
+    }
+
+    @Test
+    public void testRecommendActor1() throws Exception {
         List<String> movies1 = Arrays.asList("Airplane!", "Airplane II: The Sequel");
         List<String> movies2 = Arrays.asList("Animal House", "The Blues Brothers");
         Actor actor1 = new Actor("Robert Hays", movies1, new int[]{2, 0, 0, 2}, new double[]{0.1, 0.0, 0.0, 0.3});
@@ -71,6 +84,27 @@ public class GBARControllerTest {
                         "  \"genre\": [27.6, 7.4, 0, 52, 0, 6.6, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],\n" +
                         "  \"synergy\": 20,\n" +
                         "  \"supporter\": [\"Robert Hays\", \"John Belushi\"],\n" +
+                        "  \"plot\": \"plot is here\"\n" +
+                        "}"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    @Test
+    public void testRecommendActor2() throws Exception {
+        List<String> movies1 = Arrays.asList("Howl's Moving Castle", "Spirited Away");
+        List<String> movies2 = Arrays.asList("The World of Us", "The Table");
+        Actor actor1 = new Actor("Miazaki Hayao", movies1, new int[]{2, 0, 0, 2}, new double[]{0.1, 0.0, 0.0, 0.3});
+        Actor actor2 = new Actor("Jo Sumin", movies2, new int[]{2, 0, 0, 2}, new double[]{0.1, 0.0, 0.0, 0.3});
+        Set<Actor> actors = new HashSet<>(Arrays.asList(actor1, actor2));
+        when(gbarService.getActorByName("Miazaki Hayao")).thenReturn(actor1);
+        when(gbarService.getActorByName("Jo Sumin")).thenReturn(actor2);
+        when(recommendator.recommend((double[]) any(), anyList(), anyInt(), anyString(), anyList(), anyInt())).thenReturn(actors);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/gbar/recommend")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"genre\": [27.6, 7.4, 0, 52, 0, 6.6, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],\n" +
+                        "  \"synergy\": 20,\n" +
+                        "  \"supporter\": [\"Miazaki Hayao\", \"Jo Sumin\"],\n" +
                         "  \"plot\": \"plot is here\"\n" +
                         "}"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
--- backend/src/test/java/com/example/cse364project/controller/GFAControllerTest.java
+++ backend/src/test/java/com/example/cse364project/controller/GFAControllerTest.java
@@ -3,14 +3,13 @@ package com.example.cse364project.controller;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -18,6 +17,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import com.example.cse364project.dto.GenreRate;
 import com.example.cse364project.service.GFAService;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -36,7 +36,7 @@ public class GFAControllerTest {
     }
 
     @Test
-    void testGetGenreFrequencyWithRatings() throws Exception {
+    void testGetGenreFrequencyWithRatings1() throws Exception {
         // Mock the service response
         List<GenreRate> genreRates = Arrays.asList(new GenreRate(Arrays.asList("Action"), 3.0, 4), new GenreRate(Arrays.asList("Comedy"), 2.5, 6));
         when(gfaService.getGenreFrequencyWithRatings()).thenReturn(genreRates);
@@ -44,24 +44,79 @@ public class GFAControllerTest {
         // Perform the GET request
         mockMvc.perform(get("/gfa")
             .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andExpect(jsonPath("$.length()").value(2))
+            .andExpect(jsonPath("$.content.length()").value(2))
+            .andExpect(jsonPath("$.content[0].genres[0]").value("Action"));
     }
 
     @Test
-    void testGetGenreFrequencyWithRatingsIncludingGenre() throws Exception {
+    void testGetGenreFrequencyWithRatings2() throws Exception {
+        // Mock the service response
+        List<GenreRate> genreRates = Arrays.asList(new GenreRate(Arrays.asList("Action"), 3.0, 4));
+        when(gfaService.getGenreFrequencyWithRatings()).thenReturn(genreRates);
+
+        // Perform the GET request
+        mockMvc.perform(get("/gfa")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andDo(print())
+            .andExpect(jsonPath("$.length()").value(2))
+            .andExpect(jsonPath("$.content.length()").value(1))
+            .andExpect(jsonPath("$.content[0].genres[0]").value("Action"));
+    }
+
+    @Test
+    void testGetGenreFrequencyWithRatingsIncludingGenre1() throws Exception {
         // Mock the service response
         String genre = "Action";
         List<GenreRate> genreRates = Arrays.asList(new GenreRate(Arrays.asList("Action"), 3.0, 4), new GenreRate(Arrays.asList("Comedy"), 2.5, 6));
-        when(gfaService.getGenreFrequencyWithRatingsIncludingGenre(genre)).thenReturn(genreRates);
+        
+        List<GenreRate> filteredGenreRate = new ArrayList<>();
+        for (GenreRate genreRate : genreRates) {
+            if (genreRate.getGenres().contains(genre)) {
+                filteredGenreRate.add(genreRate);
+            }
+        }
+        
+        when(gfaService.getGenreFrequencyWithRatingsIncludingGenre(genre)).thenReturn(filteredGenreRate);
 
         // Perform the GET request
         mockMvc.perform(get("/gfa/genre/{genre}", genre)
             .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.length()").value(2))
+            .andExpect(jsonPath("$.content.length()").value(1))
+            .andExpect(jsonPath("$.content[0].genres[0]").value("Action"));
     }
 
     @Test
-    void testGetGenreFrequencyWithRatingsIncludingYear() throws Exception {
+    void testGetGenreFrequencyWithRatingsIncludingGenre2() throws Exception {
+        // Mock the service response
+        String genre = "Comedy";
+        List<GenreRate> genreRates = Arrays.asList(new GenreRate(Arrays.asList("Action"), 3.0, 4), new GenreRate(Arrays.asList("Comedy"), 2.5, 6));
+        
+        List<GenreRate> filteredGenreRate = new ArrayList<>();
+        for (GenreRate genreRate : genreRates) {
+            if (genreRate.getGenres().contains(genre)) {
+                filteredGenreRate.add(genreRate);
+            }
+        }
+        
+        when(gfaService.getGenreFrequencyWithRatingsIncludingGenre(genre)).thenReturn(filteredGenreRate);
+
+        // Perform the GET request
+        mockMvc.perform(get("/gfa/genre/{genre}", genre)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.length()").value(2))
+            .andExpect(jsonPath("$.content.length()").value(1))
+            .andExpect(jsonPath("$.content[0].genres[0]").value("Comedy"));
+    }
+
+    @Test
+    void testGetGenreFrequencyWithRatingsIncludingYear1() throws Exception {
         // Mock the service response
         int year = 2022;
         List<GenreRate> genreRates = Arrays.asList(new GenreRate(Arrays.asList("Action"), 3.0, 4), new GenreRate(Arrays.asList("Comedy"), 2.5, 6));
@@ -70,6 +125,23 @@ public class GFAControllerTest {
         // Perform the GET request
         mockMvc.perform(get("/gfa/{year}", year)
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$.content.length()").value(2));
+    }
+
+    @Test
+    void testGetGenreFrequencyWithRatingsIncludingYear2() throws Exception {
+        // Mock the service response
+        int year = 1990;
+        List<GenreRate> genreRates = Arrays.asList(new GenreRate(Arrays.asList("Action"), 3.0, 4), new GenreRate(Arrays.asList("Comedy"), 2.5, 6));
+        when(gfaService.getGenreFrequencyWithRatingsIncludingYear(year)).thenReturn(genreRates);
+
+        // Perform the GET request
+        mockMvc.perform(get("/gfa/{year}", year)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$.content.length()").value(2));
     }
 }
