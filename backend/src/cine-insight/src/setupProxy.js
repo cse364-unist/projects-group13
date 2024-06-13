f6f2c40028ab56f3bf91888f7b745d27810fd4a4
queso-gato1355 <solar767@unist.ac.kr>
2024-06-14 01:03:09 +0900

    feat : Docker 빌드 가능
    - :tada: 이제 도커 내에서 빌드할 수 있음.
    - :truck: 폴더를 옮김
    - :wastebasket: 기존 frontend 폴더는 삭제 예정

---
+++ backend/src/cine-insight/src/setupProxy.js
@@ -0,0 +1,12 @@
+//src/main/cine-insight/src/setupProxy.js
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',	// 서버 URL or localhost:설정한포트번호
+      changeOrigin: true,
+    })
+  );
+};
